5d21fa43788ac7d10cb87ac95de45c821f34716f
Munter <martin.unterreiner@posteo.de>
2019-03-31 12:47:48 +0200

    Fixed some typpos.

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
 def sayHelloWorld(name):
-    print("Hello world! ", name , " , nice to meet you.")
+    print("Hello world! ", name , ", nice to meet you.")
 
-    
-name = input("What is your name?")
-sayHelloWorld(name)
+option = True    
+
+while (option == True):
+    name = input("What is your name? Insert q to close. ")
+    if (name == "q"):
+        option = False
+    else:
+        sayHelloWorld(name)
+
